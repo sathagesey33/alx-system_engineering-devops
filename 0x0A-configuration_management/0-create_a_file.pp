79fe2d16ba1597e60ac0032889442a48723ed642
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-11-24 13:42:09 -0800

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Puppet Manifest: Creating a file in /tmp with specified permissions and content
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
+}
