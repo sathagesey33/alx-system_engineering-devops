0d3c97337cc72d27847b896962897995302ff111
sathagesey33 <abdukadirmuhanad@gmail.com>
2024-01-02 13:44:13 -0800

    Export to CSV mandatory

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+extension of 0-gather_data_from_an_API.py
+that exports data in CSV format
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_str = '{}users/{}'.format(base_url, user_id)
+    todos_str = '{}todos?userId={}'.format(base_url, user_id)
+    file = '{}.csv'.format(user_id)
+
+    res = requests.get(user_str)
+    username = res.json().get('username')
+
+    res = requests.get(todos_str)
+    tasks = []
+    for task in res.json():
+        tasks.append([user_id, username,
+                      task.get('completed'), task.get('title')])
+
+    with open(file, mode='w') as emp_file:
+        emp_writer = csv.writer(emp_file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            emp_writer.writerow(task)
